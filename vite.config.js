6570929a0ad97de716a0b27110b996431f85533f
ErenAydogan <erenaydogan@live.fr>
2024-10-16 12:59:27 +0300

    Viteconfig has been updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/cinemania/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
