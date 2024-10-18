d5edeaf527a645f3e61a2f61ee07f551d844d249
ErenAydogan <erenaydogan@live.fr>
2024-10-18 18:55:50 +0300

    Footer has been added

--- src/js/footerGoitStudents.js
+++ src/js/footerGoitStudents.js
@@ -1,3 +1,13 @@
-document.querySelector('.footer-container').addEventListener('mouseover', () => {
-    console.log("Test");
+document.querySelector('.about-link').addEventListener('click', () => {
+        document.querySelector('.about').classList.remove('hidden');
+        document.querySelector('header').classList.add('hidden')
+        document.querySelector('main').classList.add('hidden')
+        document.querySelector('.footer-container').classList.add('hidden')
 });
+
+document.querySelector('.about-x').addEventListener('click', () => {
+    document.querySelector('.about').classList.add('hidden')
+    document.querySelector('header').classList.remove('hidden')
+    document.querySelector('main').classList.remove('hidden')
+    document.querySelector('.footer-container').classList.remove('hidden')
+})
